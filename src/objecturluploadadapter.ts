7668d4e6d1e79241975b32739b33ec95e4b010f9
wit <witwitchayakarn@gmail.com>
2025-07-31 18:57:49 +0700

    Add objecturluploadadapter.ts

---
+++ src/objecturluploadadapter.ts
@@ -0,0 +1,101 @@
+import { Plugin, FileRepository } from 'ckeditor5';
+import loadImage from 'blueimp-load-image';
+
+declare global {
+  interface Window {
+    JustnoteReactWebApp?: any;
+    ReactNativeWebView?: any;
+  }
+}
+
+export default class ObjectUrlUploadAdapter extends Plugin {
+
+  static get requires() {
+    return [FileRepository];
+  }
+
+  static get pluginName() {
+    return 'ObjectUrlUploadAdapter';
+  }
+
+  init() {
+    this.editor.plugins.get(FileRepository).createUploadAdapter = loader => new Adapter(loader);
+  }
+}
+
+class Adapter {
+
+  loader: any;
+
+  constructor(loader) {
+    this.loader = loader;
+  }
+
+  upload() {
+    if (window.JustnoteReactWebApp) {
+      window.JustnoteReactWebApp.updateIsUploading(true);
+    }
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage('update:isUploading:true');
+    }
+
+    return this.loader.file.then(file => new Promise<any>((resolve, reject) => {
+      loadImage(
+        file,
+        { maxWidth: 1688, maxHeight: 1688, orientation: true, meta: true, canvas: true }
+      ).then(data => {
+        try {
+          data.image.toBlob((blob) => {
+            const objectUrl = URL.createObjectURL(blob);
+
+            if (window.JustnoteReactWebApp) {
+              window.JustnoteReactWebApp.addObjectUrlFiles(objectUrl, file.name, blob);
+              resolve({ default: objectUrl });
+              return;
+            }
+
+            if (window.ReactNativeWebView) {
+              const reader = new FileReader();
+              reader.onerror = e => reject(e);
+              reader.onload = () => {
+                const SEP = '_jUSTnOTE-sEpArAtOr_';
+                const content = reader.result;
+                window.ReactNativeWebView.postMessage(
+                  'add:objectUrlFiles:' + objectUrl + SEP + file.name + SEP + content
+                );
+                resolve({ default: objectUrl });
+              };
+              reader.readAsDataURL(blob);
+              return;
+            }
+          }, file.type);
+        } catch (e) {
+          if (window.JustnoteReactWebApp) {
+            window.JustnoteReactWebApp.updateIsUploading(false);
+          }
+          if (window.ReactNativeWebView) {
+            window.ReactNativeWebView.postMessage('update:isUploading:false');
+          }
+          reject(e);
+        }
+      }).catch(e => {
+        if (window.JustnoteReactWebApp) {
+          window.JustnoteReactWebApp.updateIsUploading(false);
+        }
+        if (window.ReactNativeWebView) {
+          window.ReactNativeWebView.postMessage('update:isUploading:false');
+        }
+        reject(e);
+      });
+    })).catch(e => {
+      if (window.JustnoteReactWebApp) {
+        window.JustnoteReactWebApp.updateIsUploading(false);
+      }
+      if (window.ReactNativeWebView) {
+        window.ReactNativeWebView.postMessage('update:isUploading:false');
+      }
+    });
+  }
+
+  abort() {}
+}
